aab47825da2a182f062e1cfce0e616c93b06bab5
Andrew Belanger <andrew.j.belanger@gmail.com>
2020-08-20 15:15:29 -0400

    ok

--- Ghost_App/models.py
+++ Ghost_App/models.py
@@ -5,4 +5,6 @@ class BoastOrRoast(models.Model):
     description = models.TextField()
     is_boast = models.BooleanField(default=False, blank=True)
     date = models.DateTimeField(default=datetime.now)
-    score = models.IntegerField(default = 0)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+    vote_score = models.IntegerField(default=0)
--- Ghost_App/views.py
+++ Ghost_App/views.py
@@ -3,7 +3,7 @@ from Ghost_App.forms import BoastOrRoastForm
 from Ghost_App.models import BoastOrRoast
 
 def index (request):
-    data = BoastOrRoast.objects.order_by('id').reverse()
+    data = BoastOrRoast.objects.order_by('-date')
     return render(request, 'index.html', {'data':data})
 
 
@@ -15,8 +15,8 @@ def add_post(request):
             new_post = BoastOrRoast.objects.create(
                 description = form.get('description'),
                 is_boast = form.get('is_boast'),
-                # date = form.get('date'),
-                score = 0
+                up_votes = 0,
+                down_votes = 0
             )
             return HttpResponseRedirect('/')
     else:
@@ -27,7 +27,8 @@ def add_post(request):
 def like_post(request, post_id):
     post_info = BoastOrRoast.objects.filter(id=post_id)
     post_info = post_info.first()
-    post_info.score += 1
+    post_info.up_votes += 1
+    post_info.vote_score = post_info.up_votes - post_info.down_votes
     post_info.save()
     return HttpResponseRedirect('/')
 
@@ -35,21 +36,24 @@ def like_post(request, post_id):
 def dislike_post(request, post_id):
     post_info = BoastOrRoast.objects.filter(id=post_id)
     post_info = post_info.first()
-    post_info.score -= 1
+    post_info.down_votes += 1
+    post_info.vote_score = post_info.up_votes - post_info.down_votes
     post_info.save()
     return HttpResponseRedirect('/')
 
 
 def boasts(request):
-    data = BoastOrRoast.objects.filter(is_boast=True)
+    data = BoastOrRoast.objects.order_by('-date')
+    data = data.objects.filter(is_boast=True)
     return render(request, 'index.html', {'data':data})
-
+# sort by time submitted
 
 def roasts(request):
-    data = BoastOrRoast.objects.filter(is_boast=False)
+    data = BoastOrRoast.objects.order_by('-date')
+    data = data.objects.filter(is_boast=False)
     return render(request, 'index.html', {'data':data})
-
+# sort by time submitted
 
 def score_rank(request):
-    data = BoastOrRoast.objects.order_by('score').reverse()
+    data = BoastOrRoast.objects.order_by('vote_score').reverse()
     return render(request, 'index.html', {'data':data})
